5ad66f87f89381b18ff6070f718cba288b34e8bf
2jdmtec <2jdmtec@bglab.local>
2023-05-02 12:14:33 -0300

    testando canvas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
     ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 150, 100);
+    ctx.fillRect(18, 18, 150, 100);
+  ctx.arc(700,18,15,0,Math.PI* 2);
+  ctx.stroke();
