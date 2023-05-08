30da9050e20593f52235b9d4b53ab266afce0773
2jdmtec <2jdmtec@bglab.local>
2023-05-08 12:10:35 -0300

    colocando novos objetos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+//retangulo
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'green';
-    ctx.fillRect(18, 18, 150, 100);
-  ctx.arc(700,18,15,0,Math.PI* 2);
+  ctx.fillStyle = 'green';
+  ctx.fillRect(18, 18, 150, 100);
+
+    //circulo
+  ctx.arc(400,80,50,0,Math.PI* 2);
   ctx.stroke();
+
+  //curva
+  ctx.beginPath();
+  ctx.arc(550, 80, 50, 180, Math.PI * 2);
+  ctx.stroke();
+
+  //linha
+  ctx.beginPath();
+  ctx.moveTo(200,100);
+  ctx.lineTo(300,100);
+  ctx.stroke();
+
+  //imagem
+  ctx.drawImage(imagem,10,10);
+var imagem = new Image();
+imagem.src = "img/neymar.jpg";
